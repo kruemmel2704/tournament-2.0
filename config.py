da3062ac3fb91d8ea72e7d6ca348f47f75493559
kruemmel2704 <lars.kruemmel@yahoo.de>
2026-02-09 23:10:06 +0000

    fix

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ class Config:
     SECRET_KEY = 'dein-geheimer-schluessel-bitte-aendern'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///tournament.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    basedir = os.path.abspath(os.path.dirname(__file__))
     UPLOAD_FOLDER = 'app/static/map_images'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
